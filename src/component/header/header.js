ef3ceefe7da45d655d0b610e108efb5919f5bec6
Xiu Hong <xiuhong.kooi977055@gmail.com>
2020-07-01 11:33:58 +0800

    Change from bootstrap NavLink to Router Link

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -4,21 +4,25 @@ import Logo from '@app/assets/logo/logo.png';
 
 import './header.scss';
 import { Navbar, Nav} from 'react-bootstrap';
+import { NavLink } from "react-router-dom";
 
 const links = [
     {
         label: 'Skills',
         href: '/skills',
+        external: false,
         uid: 1
     },
     {
         label: 'Projects',
         href: '/projects',
+        external: false,
         uid: 2
     },
     {
         label: 'Blog',
         href: 'https://blog.kooixiuhong.com',
+        external: true,
         uid: 3
     }
 ];
@@ -27,11 +31,12 @@ const links = [
 function renderLink() {
     let navs = [];
     links.forEach((elem) => {
-        let isActive = window.location.pathname === elem.href;
-        let className = (isActive) ? 'activeLink' : '';
-        navs.push(
-            <Nav.Link href={elem.href} className={className} key={elem.uid}>{elem.label}</Nav.Link>
-        )
+
+
+        let navLink = (elem.external) ? <a href={elem.href} className="ml-3 mr-3" key={elem.uid}>{elem.label}</a> :
+            <NavLink to={elem.href} className="ml-3 mr-3" activeClassName="activeLink" key={elem.uid}>{elem.label}</NavLink>
+
+        navs.push(navLink);
     });
     return navs;
 }
@@ -39,12 +44,10 @@ function renderLink() {
 
 function Header() {
     let navLinks = renderLink();
-    let isHomePageActive = window.location.pathname === '/';
-    let logoClass = isHomePageActive ? 'logoWrapActive' : 'logoWrap';
     return (
         <Navbar className="headerWrapper mt-3" expand="lg" variant="dark">
 
-            <Navbar.Brand href="/"><img className={logoClass} src={ Logo }  alt="site logo"/></Navbar.Brand>
+            <NavLink exact to="/" className="logoWrap" activeClassName="logoWrapActive"><img src={ Logo } alt="site logo"/></NavLink>
             <Navbar.Toggle aria-controls="basic-navbar-nav" id="primary-navbar-toggle" />
 
             <Navbar.Collapse id="basic-navbar-nav" className="ml-4 pt-2 pb-2 navBar">
