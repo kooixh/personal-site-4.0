cc5410058eb0271c76dacb51891931d19e3a2cdf
Xiu Hong <xiuhong.kooi977055@gmail.com>
2020-07-14 10:18:49 +0800

    Use fontawesome icon for dark mode toggler

--- src/components/dark.mode/toggle/dark.mode.toggle.js
+++ src/components/dark.mode/toggle/dark.mode.toggle.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import useDarkMode from "use-dark-mode";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
+import './dark.mode.toggle.scss';
 
 function renderToggleButton(darkMode) {
-    if (darkMode.value)
-        return <button onClick={darkMode.toggle}> Click for light </button>;
-    else
-        return <button onClick={darkMode.toggle}> Click for dark </button>;
+    if (darkMode.value) {
+        return (
+            <button onClick={darkMode.toggle} title="Turn on the light">
+                <FontAwesomeIcon icon={faSun} size="lg"/>
+            </button>
+        )
+    } else {
+        return (
+            <button onClick={darkMode.toggle} title="Turn off the light">
+                <FontAwesomeIcon icon={faMoon} size="lg"/>
+            </button>
+        )
+    }
 }
 
 export function DarkModeToggle() {
