854c281ff82b2e2f8927e1c1e8877f5bb3fee572
Xiu Hong <xiuhong.kooi977055@gmail.com>
2020-07-15 09:51:50 +0800

    Split JobCard component into multiple smaller components

--- src/components/card/job/card.job.js
+++ src/components/card/job/card.job.js
@@ -1,21 +1,32 @@
 import React from 'react';
 
+function JobName({ props }) {
+    return (
+        <div className="col-7">
+            <h3 className="mb-0 title">{props.title}</h3>
+            <div className="mb-3">
+                <a className="subheading primary-link" href={props.jobLink} title={props.jobLinkTitle}>
+                    {props.company}
+                </a>
+            </div>
+            <p>{props.description}</p>
+        </div>
+    )
+}
+
+function JobDate({ props }) {
+    return (
+        <div className="col text-center">
+            <p className="inner-heading font-weight-bold d-inline">{props.date}</p>
+        </div>
+    )
+}
+
 export function JobCard({ props }) {
     return (
         <div className="row mb-5">
-            <div className="col-7">
-                <h3 className="mb-0 title">{props.title}</h3>
-                <div className="mb-3">
-                    <a className="subheading primary-link" href={props.jobLink} title={props.jobLinkTitle}>
-                        {props.company}
-                    </a>
-                </div>
-                <p>{props.description}</p>
-            </div>
-
-            <div className="col text-center">
-                <p className="inner-heading font-weight-bold d-inline">{props.date}</p>
-            </div>
+            <JobName props={props}/>
+            <JobDate props={props}/>
         </div>
     );
 }
